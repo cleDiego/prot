b19271ef209837a00bdb7b8e6b2391434471bb81
cleDiego <cleversondesign@hotmail.com>
2023-02-09 14:42:56 -0300

    *

--- main.js
+++ main.js
@@ -1,38 +1,9 @@
 function initInterface() {
-    function include(file = null, type = 'script', appendIn = 'head', id = null, name = null, value = null, content = null, html = null) {
+    function include(type = 'script', appendIn = 'head', html = null, attr = []) { //id = null, name = null, value = null, content = null) {
         var elem;
-        switch (type) {
-            case 'head' || 'body':
-                elem = document.createElement(type);
-                document.getElementsByTagName(appendIn).item(0).appendChild(elem);
-                break;
-            case 'title':
-                elem = document.createElement(type);
-                elem.innerHTML = html;
-                document.getElementsByTagName(appendIn).item(0).appendChild(elem);
-                break;
-            case 'script':
-                elem = document.createElement(type);
-                elem.src = file;
-                elem.type = 'text/javascript';
-                elem.defer = true;
-                document.getElementsByTagName(appendIn).item(0).appendChild(elem);
-                break;
-            case 'link':
-                elem = document.createElement(type);
-                elem.href = file;
-                elem.rel = 'stylesheet';
-                document.getElementsByTagName(appendIn).item(0).appendChild(elem);
-                break;
-            case 'meta':
-                elem = document.createElement(type);
-                elem.name = name;
-                elem.content = content;
-                document.getElementsByTagName(appendIn).item(0).appendChild(elem);
-                break;
-            default:
-                break;
-        }
+        elem = document.createElement(type);
+        for ([k, v] of Object.entries(attr)) elem.setAttribute(k, v);
+        document.getElementsByTagName(appendIn).item(0).appendChild(elem);
     }
 
     $(document).ready(function () {
@@ -60,18 +31,18 @@ function initInterface() {
         //pausar o refresh
         window.stop();
 
-        include(null, 'meta', 'head', null, 'viewport', null, 'width=device-width, initial-scale=1');
-        include(null, 'meta', 'head', null, null, null, 'text/html; charset=utf-8');
-        include(null, 'title', 'head', null, null, null, null, 'Caixa de Trabalho');
-        include('https://code.jquery.com/jquery-3.6.3.min.js', 'script', 'head');
-        include('https://code.jquery.com/ui/1.13.2/jquery-ui.min.js', 'script', 'head');
-        include('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css', 'link', 'head');
-        include('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js', 'script', 'body');
+        include('meta', 'head', null, { name: 'viewport', content: 'width=device-width, initial-scale=1' });
+        include('meta', 'head', null, { charset: 'utf-8' });
+        include('title', 'head', 'Caixa de Trabalho', { charset: 'utf-8' });
 
-        include('https://cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css', 'link', 'head');
-        include('https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js', 'script', 'body');
+        include('script', 'body', null, { src: 'https://code.jquery.com/jquery-3.6.3.min.js' });
+        include('script', 'body', null, { src: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js' });
+        include('script', 'body', null, { src: 'https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js' });
+        include('script', 'body', null, { src: 'https://code.jquery.com/ui/1.13.2/jquery-ui.min.js' });
 
-        include('https://clediego.github.io/prot/main.css?v='+Date.now(), 'link', 'head');
+        include('link', 'head', null, { href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css' });
+        include('link', 'head', null, { href: 'https://cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css' });
+        include('link', 'head', null, { href: 'https://clediego.github.io/prot/main.css?v='+Date.now() });
 
         //carregar a nova interface
         $('body').load('https://clediego.github.io/prot/main.html?v='+Date.now(), function () {
