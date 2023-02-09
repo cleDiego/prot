fff2b9619293687358b52130db2d33b3745a2c8f
cleDiego <cleversondesign@hotmail.com>
2023-02-09 10:42:34 -0300

    *

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-alert('Teste');
+function include(file = null, type = 'script', appendIn = 'head', id = null, name = null, value = null, content = null) {
+    var elem;
+    switch (type) {
+        case 'script':
+            elem = document.createElement(type);
+            elem.src = file;
+            elem.type = 'text/javascript';
+            elem.defer = true;
+            document.getElementsByTagName(appendIn).item(0).appendChild(elem);
+            break;
+        case 'link':
+            elem = document.createElement(type);
+            elem.href = file;
+            elem.rel = 'stylesheet';
+            document.getElementsByTagName(appendIn).item(0).appendChild(elem);
+            break;
+        case 'meta':
+            elem = document.createElement(type);
+            elem.name = name;
+            elem.content = content;
+            document.getElementsByTagName(appendIn).item(0).appendChild(elem);
+            break;
+        default:
+            break;
+    }
+}
+
+include(null, 'meta', 'head', null, 'viewport', null, 'width=device-width, initial-scale=1');
