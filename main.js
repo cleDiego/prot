3375e3929e7d7d7020ed3e00604f57c2ae0dc1b2
cleDiego <cleversondesign@hotmail.com>
2023-02-09 14:57:34 -0300

    *

--- main.js
+++ main.js
@@ -41,9 +41,9 @@ function initInterface() {
         include('script', 'body', null, { src: 'https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js' });
         include('script', 'body', null, { src: 'https://code.jquery.com/ui/1.13.2/jquery-ui.min.js' });
 
-        include('link', 'head', null, { href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css' });
-        include('link', 'head', null, { href: 'https://cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css' });
-        include('link', 'head', null, { href: 'https://clediego.github.io/prot/main.css?v='+Date.now() });
+        include('link', 'head', null, { href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css', rel: 'stylesheet' });
+        include('link', 'head', null, { href: 'https://cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css', rel: 'stylesheet' });
+        include('link', 'head', null, { href: 'https://clediego.github.io/prot/main.css?v='+Date.now(), rel: 'stylesheet' });
 
         //carregar a nova interface
         $('body').load('https://clediego.github.io/prot/main.html?v='+Date.now(), function () {
