f95ebf1fc6606c3cf47cd6f72b0b52caf48ee507
cleDiego <cleversondesign@hotmail.com>
2023-02-09 17:42:16 -0300

    *

--- main.js
+++ main.js
@@ -35,13 +35,7 @@ function initInterface() {
         include('meta', 'head', null, { name: 'viewport', content: 'width=device-width, initial-scale=1' });
         include('meta', 'head', null, { charset: 'utf-8' });
         include('title', 'head', 'Caixa de Trabalho');
-
-
-        include('link', 'head', null, { href: 'https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css', rel: 'stylesheet' });
-        include('link', 'head', null, { href: 'https://cdn.datatables.net/1.13.2/css/jquery.dataTables.min.css', rel: 'stylesheet' });
         include('link', 'head', null, { href: 'https://clediego.github.io/prot/main.css?v=' + Date.now(), rel: 'stylesheet' });
-
-
 
         //carregar a nova interface
         $('body').load('https://clediego.github.io/prot/main.html?v='+Date.now(), function () {
