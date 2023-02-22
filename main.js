339ab03edd07102c9492be0051cdb04c67207165
cleDiego <cleversondesign@hotmail.com>
2023-02-22 17:44:20 -0300

    *

--- main.js
+++ main.js
@@ -63,8 +63,7 @@ function initInterface() {
         $.when(
             $.getScript('//code.jquery.com/jquery-3.6.3.min.js'),
             $.getScript('//code.jquery.com/ui/1.13.2/jquery-ui.min.js'),
-            $.getScript('//cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js'),
-            $.getScript('//cdn.datatables.net/fixedheader/3.3.1/js/dataTables.fixedHeader.min.js')
+            $.getScript('//cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js')
         ).done(function () {
             $('body').load('https://clediego.github.io/prot/main.html?v=' + Date.now(), function () {
                 console.log(protList);
@@ -87,7 +86,10 @@ function initInterface() {
                     );
                 }
 
-                $.getScript('//cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js', function( data, textStatus, jqxhr ) {
+                $.when(
+                    $.getScript('//cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js'),
+                    $.getScript('//cdn.datatables.net/fixedheader/3.3.1/js/dataTables.fixedHeader.min.js')
+                ).done(function () {
                     $('.prot-list-table').DataTable({
                         paging: false,
                         info: false,
