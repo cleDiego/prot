f13309b8b66a75cc75f08b6ba4f899f3b60ab107
cleDiego <cleversondesign@hotmail.com>
2023-02-22 10:04:50 -0300

    *

--- main.js
+++ main.js
@@ -64,10 +64,7 @@ function initInterface() {
             $.getScript('https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js'),
             $.getScript('https://cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js'),
             $.getScript('https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/dist/js/bootstrap-select.min.js'),
-            $.getScript('https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/js/bootstrap-select.js'),
-            $.Deferred(function( deferred ){
-                $( deferred.resolve );
-            })
+            $.getScript('https://cdn.jsdelivr.net/npm/bootstrap-select@1.14.0-beta2/js/bootstrap-select.js')
         ).done(function () {
             $('body').load('https://clediego.github.io/prot/main.html?v=' + Date.now(), function () {
                 console.log(protList);
@@ -108,9 +105,6 @@ function initInterface() {
                         {orderable: true}
                     ]
                 });
-
-
-                $('.selectpicker').selectpicker();
 
             });
         });
