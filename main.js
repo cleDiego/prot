03f4cfd493afe0a537322e1674afb05f8089683a
cleDiego <cleversondesign@hotmail.com>
2023-02-23 09:01:46 -0300

    *

--- main.js
+++ main.js
@@ -64,7 +64,10 @@ function initInterface() {
             $.getScript('//code.jquery.com/jquery-3.6.3.min.js'),
             $.getScript('//code.jquery.com/ui/1.13.2/jquery-ui.min.js'),
             $.getScript('//cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js'),
-            $.getScript('//cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js')
+            $.getScript('//cdn.datatables.net/1.13.2/js/jquery.dataTables.min.js'),
+            $.Deferred(function( deferred ){
+                $( deferred.resolve );
+            })
         ).done(function () {
             $('body').load('https://clediego.github.io/prot/main.html?v=' + Date.now(), function () {
                 console.log(protList);
